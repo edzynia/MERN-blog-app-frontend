866e00885e2bce8156adc1f34283364fe2361edf
Eugenia Dzynia <eugenia.dzynia@edaider.com>
2023-03-17 10:09:15 +0100

    updated fake data

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -49,14 +49,14 @@ export const FullPost = () => {
         items={[
           {
             user: {
-              fullName: 'Вася Пупкин',
+              fullName: 'Den Drown',
               avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
             },
-            text: 'Это тестовый комментарий 555555',
+            text: 'Test comment ',
           },
           {
             user: {
-              fullName: 'Иван Иванов',
+              fullName: 'Shrek',
               avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
             },
             text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,14 +60,14 @@ export const Home = () => {
             items={[
               {
                 user: {
-                  fullName: 'Вася Пупкин',
+                  fullName: 'Den Brown',
                   avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
                 },
-                text: 'Это тестовый комментарий',
+                text: 'This is test comment',
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
+                  fullName: 'Abas Abas',
                   avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
                 },
                 text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
